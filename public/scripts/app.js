bde024df45711e3617bde41284d6fa02dd6d8463
janeosaur <janewie@gmail.com>
2017-04-15 23:35:19 -0700

    completed step 1 of sprint 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -39,8 +39,25 @@ sampleAlbums.push({
 
 $(document).ready(function() {
   console.log('app.js loaded!');
+  sampleAlbums.forEach(renderAlbum);
+
+  $.ajax({
+    method: 'get',
+    url: '/api/albums',
+    success: handleSuccess,
+    error: handleError
+  })
 });
 
+function handleSuccess (albums) {
+  albums.forEach(function(album) {
+    renderAlbum(album);
+  });
+};
+
+function handleError(err){
+  console.log('There has been an error: ', err);
+}
 
 
 
@@ -48,5 +65,41 @@ $(document).ready(function() {
 // this function takes a single album and renders it to the page
 function renderAlbum(album) {
   console.log('rendering album:', album);
-
+  var albumHtml = (`
+   <div class="row album">
+     <div class="col-md-10 col-md-offset-1">
+       <div class="panel panel-default">
+         <div class="panel-body">
+         <!-- begin album internal row -->
+           <div class='row'>
+             <div class="col-md-3 col-xs-12 thumbnail album-art">
+               <img src="images/800x800.png" alt="album image">
+             </div>
+             <div class="col-md-9 col-xs-12">
+               <ul class="list-group">
+                 <li class="list-group-item">
+                   <h4 class='inline-header'>Album Name:</h4>
+                   <span class='album-name'>${album.name}</span>
+                 </li>
+                 <li class="list-group-item">
+                   <h4 class='inline-header'>Artist Name:</h4>
+                   <span class='artist-name'>${album.artistName}</span>
+                 </li>
+                 <li class="list-group-item">
+                   <h4 class='inline-header'>Released date:</h4>
+                   <span class='album-releaseDate'>${album.releaseDate}</span>
+                 </li>
+               </ul>
+             </div>
+           </div>
+           <!-- end of album internal row -->
+           <div class='panel-footer'>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+   <!-- end one album -->
+ `);
+ $('#albums').prepend(albumHtml);
 }
