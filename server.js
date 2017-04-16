903c6daabbec8a6689da99d7f1079a832a68bd44
janeosaur <janewie@gmail.com>
2017-04-15 23:12:10 -0700

    completed step 3 of sprint 0

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static('public'));
+
+
 app.get('/', function(req, res) {
   res.sendFile('views/index.html', {root: __dirname});
 })
