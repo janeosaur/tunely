8857f780683f39bde093faa63b3bb2e8be00033c
janeosaur <janewie@gmail.com>
2017-04-15 23:16:49 -0700

    completed step 5 of spring 0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var db = require('./models');
+var controllers = require('./controllers');
 
 var app = express();
 
@@ -11,5 +12,7 @@ app.use(express.static('public'));
 app.get('/', function(req, res) {
   res.sendFile('views/index.html', {root: __dirname});
 })
+
+app.get('/api', controllers.api.index);
 
 app.listen(3000);
